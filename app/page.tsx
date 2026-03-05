51eae889b23553982e1991551d266f0c41e3f3a9
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-03-05 20:34:12 +0200

    Update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Projects } from "@/components/Projects";
 import { Skills } from "@/components/Skills";
+import { Experience } from "@/components/Experience";
+import { Contact } from "@/components/Contact";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-full">
+    <div className="flex flex-col items-center justify-center w-full">
       <Hero />
       <About />
       <Projects />
       <Skills />
-    </main>
+      <Experience />
+      <Contact />
+    </div>
   );
 }
